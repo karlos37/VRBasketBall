57ed082b5f4c20c7e798f5fcfc5b2aa55fb22f55
Vedansh Surjan <vedansh008@gmail.com>
2024-05-02 12:39:13 -0500

    Added basic court scene and basketballs bouncing
    Signed-off-by: Vedansh Surjan <vedansh008@gmail.com>

---
+++ Assets/Scripts/AddBasket.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro;
+
+public class AddBasket : MonoBehaviour
+{
+
+    [SerializeField]
+    [Tooltip("Add the GameManager GameObject here")]
+    private CountBaskets countBaskets;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if (other.gameObject.layer != 3)
+        // {
+            countBaskets.IncreaseScore();
+        // }
+    }
+}
